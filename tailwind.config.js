505eba4bf53b89fa848045b9bd9bc83f75c2e226
zakzakst <e1433104@gmail.com>
2022-07-28 07:51:25 +0900

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  content: [
+    './dist/**/*.{html,js}',
+    './node_modules/flowbite/**/*.js',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#f00',
+      },
+      backgroundImage: {
+        'primary-gradient': 'linear-gradient(to right, #f00, #fff)',
+      },
+      borderRadius: {
+        DEFAULT: '8px',
+        // 'lg': '24px',
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        },
+      },
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+      },
+    },
+  },
+  plugins: [
+    // require('@tailwindcss/forms'),
+    // require('flowbite/plugin'),
+  ],
+}
